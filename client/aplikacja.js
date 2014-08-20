d123aadc46dab5d8fbc1ce5348892e7e81360ecb
apajakowski <adi.grudziadz@gmail.com>
2014-08-20 23:58:45 +0200

    Update

--- client/aplikacja.js
+++ client/aplikacja.js
@@ -5,21 +5,25 @@ Template.messages.messages = function () {
 
 Template.input.events = {
   'keydown input#message' : function (event) {
-    if (event.which == 13) { // 13 is the enter key event
-      var name = document.getElementById('name');;
+    if (event.which == 13) 
+    { // 13 is the enter key event
+      var name = document.getElementById('name');
       var message = document.getElementById('message');
-      if (name.value != '' && message.value != '') {
-        Messages.insert({
-          name: name.value,
-          message: message.value,
-          time: Date.now(),
-        });
-        message.value = '';
+        if(name.value.length > 0 && message.value.length >= 1 && message.value.length <= 25) { 
+        Messages.insert({ 
+          name: name.value, 
+          message: message.value, 
+          time: Date.now(), 
+        }); 
+        message.value = ''; 
+         } else { 
+      if (name.value.length < 1) 
+         alert('Wpisz imie!'); 
+     if (message.value.length < 1 || message.value.length > 25) 
+        alert('Wiadomosc za krotka albo ma wiecej niz 25 znakow!');
       }
-      if (name.value == '')
-       {
-        alert('Nie wpisales imienia!'); 
-       }
+      
     }
   }
 }
+
